bc22debea660d45539759a396d7e07ce48b8cf77
Merro Ab <merro9@gmail.com>
2015-08-23 18:23:05 +1000

    Removed page.php
    One PHP file for the bulk of page layout over smaller separate pages

--- index.php
+++ index.php
@@ -1,22 +1,48 @@
 <?php defined('_JEXEC') or die;
 $template = 'templates/'.$this->template;
 
-include_once $template.'/logic.php';
-include_once $template.'/head.php';
-include_once $template.'/page.php';
-
 /* detect mobile devices - http://mobiledetect.net/ */
 require_once 'Mobile_Detect.php';
-$detect = new Mobile_Detect;
-$mobile = ($detect->isMobile() && !$detect->isTablet());
 
-?><!doctype html>
-<html lang="<?php echo $this->language; ?>" class="no-js">
+include_once $template.'/logic.php';
+include_once $template.'/head.php';
+?>
+<!doctype html>
+<html lang="<?php echo $siteLang; ?>" class="no-js">
 <?php echo $htmlHead; ?>
 <?php flush(); ?>
-	<body class="site" role="document">
-		<?php echo $compatWarning; ?>
-		
+	<body class="<?php echo $bodyclass; ?>" role="document">
+
+		<!--[if lt IE 9]>
+		<div class="alert alert-warning alert-dismissible text-center" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			<p>You are using an <strong>outdated</strong> browser. Please <a target="_blank" class="alert-link" href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		</div>
+		<![endif]-->
+
+		<a id="skippy" class="sr-only sr-only-focusable" href="#content">
+			<div class="container">
+				<span class="skiplink-text">Skip to main content</span>
+			</div>
+		</a>
+
+		<header class="navbar navbar-static-top" id="top" role="banner">
+		  <div class="container">
+		    <div class="navbar-header">
+		      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#inti-navbar" aria-controls="inti-navbar" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a href="/" class="navbar-brand"><?php echo $sitename ?></a>
+		    </div>
+		    <nav id="inti-navbar" class="collapse navbar-collapse" role="navigation">
+		      <jdoc:include type="modules" name="menu" />
+		    </nav>
+		  </div>
+		</header>
+
 		<?php echo $scripts; ?>
 	</body>
 </html>
